f53756fb5de8255c047ecc996fcc02be5e09c680
Schroedingberg <aaronrebmann@gmail.com>
2026-01-11 15:31:59 +0100

    infra: adjust deployment tasks to allow specifying ssh port

--- server-setup.sh
+++ server-setup.sh
@@ -78,49 +78,47 @@ EOD
 chmod 440 /etc/sudoers.d/restart-app
 
 # Firewall
-ufw allow OpenSSH
-ufw --force enable
 
 # Web dependencies
-apt-get -y install nginx
-snap install core
-snap refresh core
-snap install --classic certbot
-ln -s /snap/bin/certbot /usr/bin/certbot
+# apt-get -y install nginx
+# snap install core
+# snap refresh core
+# snap install --classic certbot
+# ln -s /snap/bin/certbot /usr/bin/certbot
 
-# Nginx
-rm /etc/nginx/sites-enabled/default
-cat > /etc/nginx/sites-available/app << EOD
-server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
-    server_name _;
-    gzip_types text/plain text/css application/json application/javascript text/xml application/xml application/xml+rss text/javascript;
-    root /home/app/target/resources/public;
-    location / {
-        try_files \$uri \$uri/index.html @resources;
-    }
-    location @resources {
-        root /home/app/resources/public;
-        try_files \$uri \$uri/index.html @proxy;
-    }
-    location @proxy {
-        proxy_pass http://localhost:8080;
-        proxy_http_version 1.1;
-        proxy_set_header Host \$host;
-        proxy_set_header Upgrade \$http_upgrade;
-        proxy_set_header Connection "Upgrade";
-        proxy_set_header X-Real-IP \$remote_addr;
-    }
-}
-EOD
-ln -s /etc/nginx/sites-{available,enabled}/app
+# # Nginx
+# rm /etc/nginx/sites-enabled/default
+# cat > /etc/nginx/sites-available/app << EOD
+# server {
+#     listen 80 default_server;
+#     listen [::]:80 default_server;
+#     server_name _;
+#     gzip_types text/plain text/css application/json application/javascript text/xml application/xml application/xml+rss text/javascript;
+#     root /home/app/target/resources/public;
+#     location / {
+#         try_files \$uri \$uri/index.html @resources;
+#     }
+#     location @resources {
+#         root /home/app/resources/public;
+#         try_files \$uri \$uri/index.html @proxy;
+#     }
+#     location @proxy {
+#         proxy_pass http://localhost:8080;
+#         proxy_http_version 1.1;
+#         proxy_set_header Host \$host;
+#         proxy_set_header Upgrade \$http_upgrade;
+#         proxy_set_header Connection "Upgrade";
+#         proxy_set_header X-Real-IP \$remote_addr;
+#     }
+# }
+# EOD
+# ln -s /etc/nginx/sites-{available,enabled}/app
 
-# Firewall
-ufw allow "Nginx Full"
+# # Firewall
+# ufw allow "Nginx Full"
 
 # Let's encrypt
-certbot --nginx
+# certbot --nginx
 
 # App dependencies
 # If you need to install additional packages for your app, you can do it here.
